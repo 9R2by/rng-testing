7b536ec3f55d579772a397bda6bfe1fb24316a1d
9R2by <37716834+9R2by@users.noreply.github.com>
2023-11-04 23:25:55 +0100

    Update main.c

--- dieharder_lea_testing/main.c
+++ dieharder_lea_testing/main.c
@@ -12,7 +12,7 @@ int main(void){
         fprintf(file, "# C file generation pow(2,32)  seed = 0\n");
         fprintf(file, "# ==================================================================\n");
         fprintf(file, "type: d\n");
-        fprintf(file, "count: 4294967295\n");
+        fprintf(file, "count: 17179869180\n"); //4294967295*4
         fprintf(file, "numbit: 32\n");
         for(u = 0; u <= 0xFFFFFFFF; u++){
                 fprintf(file, "0\n0\n0\n%u\n", u);
