8b342e95aa622379a8fe986068e1048aa2692c82
9R2by <37716834+9R2by@users.noreply.github.com>
2023-11-09 20:36:23 +0100

    Create test_all.sh

---
+++ test_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+./dieharder_gen_and_testing.sh
+
+cd dieharder_lea_input_testing
+
+./start.sh
+
+cd ..
+
+cd dieharder_lea_output_testing_0_0_0_x
+
+./start.sh
+
+cd ..
+
+cd dieharder_lea_output_testing_x_0_0_0
+
+./start.sh
